76b0e3c643357100b166d939ccf72c2c46f981c5
FatinYazim <fatin_yazim@yahoo.com>
2023-07-25 16:47:39 +0800

    test

--- src/component/CTA.jsx
+++ src/component/CTA.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import cta from '../assets/CTA.png';
+import cta from '../assets/cta.png';
 
 const CTA = () => {
   return (
     <div className='w-full bg-[#E9F8F3] py-24'>
-      <div className='md:max-w-[1480px] m-auto grid md:grid-cols-2 gap-8 max-w-[600px] items-center  px-4 md:px-0'>
+    <div className='md:max-w-[1480px] m-auto grid md:grid-cols-2 max-w-[600px] gap-8  px-4 md:px-0 items-center'>
         
-    <img src={cta} className="w-[650px] mx-auto" />
+    <img  src={cta} className='w-[500px] mx-auto'/>
 
 
         <div >
